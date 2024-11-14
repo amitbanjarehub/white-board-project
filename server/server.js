cf069d8840805c850a19741b258bdc6dcf02ccde
Amit Banjare <amitbanjare79@gmail.com>
2024-11-14 14:51:32 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -1,38 +1,41 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var http = require('http').createServer(app);
-var cors = require('cors');
+var http = require("http").createServer(app);
+var cors = require("cors");
 const { Server } = require("socket.io");
 
-
-
 // Set up CORS for Express server
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:3000", // Allow requests from this origin
     methods: ["GET", "POST"],
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
 // Set up Socket.io with CORS
 const io = new Server(http, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
+io.on("connection", (socket) => {
+  socket.on("canvas-data", (data) => {
+    socket.broadcast.emit("canvas-data", data);
+  });
 
+  socket.on("clear-canvas", () => {
+    io.emit("clear-canvas");
+  });
 
-io.on('connection', (socket) => {
-    console.log("user online");
-
-    socket.on('canvas-data', (data) => {
-        socket.broadcast.emit('canvas-data', data);
-    });
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
 });
-
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
 http.listen(server_port, () => {
-    console.log("Started on:" + server_port);
+  console.log("Started on:" + server_port);
 });
